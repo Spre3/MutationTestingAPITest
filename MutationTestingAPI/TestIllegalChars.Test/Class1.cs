e53b11d9505c13d16e2d76d1476ced904a0ad44c
Roberto Silva <r>
2018-08-10 13:30:44 +0100

    Added projects with illegal chars error

---
+++ MutationTestingAPI/TestIllegalChars.Test/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestIllegalChars.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            var a = 1+1;
+            Assert.AreEqual(a, 2);
+        }
+    }
+}
---
+++ MutationTestingAPI/TestIllegalChars/Controllers/BaseApiController.cs
@@ -0,0 +1,25 @@
+﻿namespace TestIllegalChars.Controllers
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using System.Web.Http.Results;
+
+    public class BaseApiController : ApiController
+    {
+        protected ResponseMessageResult CreatedWithWarnings()
+        {
+            var newResponse = Request.CreateResponse(
+                   HttpStatusCode.Created,
+                   new
+                   {
+                       a = ""
+                   });
+
+            return new ResponseMessageResult(newResponse);
+        }
+
+
+
+    }
+}
